ec538bb504626d9e246d76329155330062141382
hctrung2k4 <hctrung2k4@gmail.com>
2024-12-01 21:16:57 +0700

    Add project files.

---
+++ Async_Await/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void DoSomeThing(int seconds, string message)
+    {
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.WriteLine($"{message} {i}");
+            Thread.Sleep(1000);
+
+        }
+    }
+    static void Main(string[] args)
+    {
+        DoSomeThing(5, "Hello World");
+        Console.WriteLine("Hello World"); 
+    }
+}
